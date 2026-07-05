156b1c887d7c49eb21f954626eba66b01fab4244
Oleg Larkin <oleg.larkin@nordigy.ru>
2026-07-05 16:53:54 +0300

    feat: rm proj files

--- init_misc_files.sh
+++ init_misc_files.sh
@@ -2,6 +2,11 @@
 
 echo "copying misc files"
 
+mkdir -p Src
+mkdir -p Inc
+mkdir -p Startup
+mkdir -p Linker_script
+
 wget https://raw.githubusercontent.com/STMicroelectronics/STM32CubeF4/refs/heads/master/Projects/STM32446E_EVAL/Templates/Src/stm32f4xx_it.c -O Src/stm32f4xx_it.c
 wget https://raw.githubusercontent.com/STMicroelectronics/STM32CubeF4/refs/heads/master/Projects/STM32446E_EVAL/Templates/Inc/main.h -O Inc/main.h
 wget https://raw.githubusercontent.com/STMicroelectronics/STM32CubeF4/refs/heads/master/Projects/STM32446E_EVAL/Templates/Inc/stm32f4xx_it.h -O Inc/stm32f4xx_it.h
@@ -9,6 +14,7 @@ wget https://raw.githubusercontent.com/STMicroelectronics/STM32CubeF4/refs/heads
 wget https://raw.githubusercontent.com/STMicroelectronics/STM32CubeF4/refs/heads/master/Projects/STM32F411RE-Nucleo/Templates/Src/main.c -O Src/main.c
 wget https://raw.githubusercontent.com/STMicroelectronics/STM32CubeF4/refs/heads/master/Projects/STM324xG_EVAL/Templates/STM32CubeIDE/Example/User/sysmem.c -O Src/sysmem.c
 wget https://raw.githubusercontent.com/STMicroelectronics/STM32CubeF4/refs/heads/master/Projects/STM32F411RE-Nucleo/Templates/STM32CubeIDE/STM32F411RETX_FLASH.ld -O Linker_script/linker_script.ld
+wget https://raw.githubusercontent.com/STMicroelectronics/STM32CubeF4/89e6d4466578bc9eab83de8fcd1e397ceb5e5cc9/Projects/STM32F411RE-Nucleo/Templates/STM32CubeIDE/Example/Startup/startup_stm32f411retx.s -O Startup/startup.s
 
 echo "copy misc complete"
 
